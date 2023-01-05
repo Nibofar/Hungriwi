70c077d2e415a8d4fb08e9d85230c28d5cabaa5b
Nibofar <quentin.bouletbenac@gmail.com>
2023-01-05 11:36:02 +0100

    gestion arbre et insecte 1

--- Assets/TreeSpawn.cs
+++ Assets/TreeSpawn.cs
@@ -11,19 +11,28 @@ public class TreeSpawn : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
             SpawnPrefab();
-
+        if (Input.GetKeyDown(KeyCode.F))
+            GameManager.Instance.ResetTree();
     }
     public void SpawnPrefab()
     {
-        int j = Random.Range(5, 8);
+        int j = Random.Range(GameManager.Instance.treeMin, GameManager.Instance.treeMax);
         for (int i = 0; i <= j; i++)
         {
-            float x = Random.Range(-9, 9);
-            float y = Random.Range(-5, 5);
-            Instantiate(tree, new Vector2(x, y), Quaternion.identity);
+            Vector2 place = GenerateVector();
+            GameManager.Instance.SetPropsData((int)place.x + GameManager.Instance.sizeMapX, (int)place.y + GameManager.Instance.sizeMapY);
+            GameManager.Instance.TreeList.Add(Instantiate(tree, place, Quaternion.identity));
 
         }
 
     }
-
+     Vector2 GenerateVector() {
+        int x = Random.Range(-GameManager.Instance.sizeMapX, GameManager.Instance.sizeMapX);
+        int y = Random.Range(-GameManager.Instance.sizeMapY, GameManager.Instance.sizeMapY);
+        Vector2 temp = new Vector2(x, y);
+        if (GameManager.Instance.propsData[x + GameManager.Instance.sizeMapX, y + GameManager.Instance.sizeMapY]) {
+            temp = GenerateVector();
+        }
+        return temp;
+    }
 }
