7b63e9319840da5cc87f2f26a61a30ac14ac1e73
SkyLaugh987 <59956060+SkyLaugh987@users.noreply.github.com>
2023-01-05 17:55:03 +0100

    sun chasing

---
+++ Assets/Scripts/RayonSoleil.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayonSoleil : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2.5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            moveDirection = direction;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+    }
+}
