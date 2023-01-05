9a93f20bc809276fe9dc9881f861a0aae96fed55
Frédéric Deiber <frederic.deiber@gmail.com>
2023-01-05 22:14:55 +0100

    map shader

--- Assets/Scripts/ZoneManager.cs
+++ Assets/Scripts/ZoneManager.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class ZoneManager : MonoBehaviour {
     [SerializeField] Transform center;
-    [SerializeField] [Min(0.01f)]       float radius = 2.0f;
-    [SerializeField] [Min(0.01f)]       float tile = 1.0f;
-    [SerializeField] [Range(0f, 1f)]    float step = 0.05f;
-    [SerializeField] [Min(0.01f)]       float edgeWidth = 1f;
+    [SerializeField] [Min(0.01f)]       float radius = 3.0f;
+    [SerializeField] [Min(0.01f)]       float tile = .4f;
+    [SerializeField] [Range(0.01f, 1f)]    float step = 0.01f;
+    [SerializeField] [Range(0.01f, 1f)]    float smoothStep = 0.06f;
+    [SerializeField] [Min(0.01f)]       float edgeWidth = 0.01f;
     [SerializeField] [Min(0.01f)]       float noiseSpeed = 0.1f;
-    [SerializeField] [Min(0.01f)]       float blurRadius = 2.0f;
-    [SerializeField] [Min(0.01f)]       float blurIntensity = 2.0f;
     [SerializeField] 
     [ColorUsage(true, true)] Color edgeColor = Color.white;
 
@@ -17,10 +16,9 @@ public class ZoneManager : MonoBehaviour {
         Shader.SetGlobalFloat("_Radius", radius);
         Shader.SetGlobalFloat("_Tile", tile);
         Shader.SetGlobalFloat("_Step", step);
+        Shader.SetGlobalFloat("_SmoothStep", smoothStep);
         Shader.SetGlobalFloat("_EdgeWidth", edgeWidth);
         Shader.SetGlobalFloat("_NoiseSpeed", noiseSpeed);
-        Shader.SetGlobalFloat("_BlurRadius", blurRadius);
-        Shader.SetGlobalFloat("_BlurIntensity", blurIntensity);
         Shader.SetGlobalColor("_EdgeColor", edgeColor);
     }
 }
